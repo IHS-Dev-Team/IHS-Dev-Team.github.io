8b3b711650109c6799c09a7e5a796e6ee30bac55
samcatterall <samuel.catterall@icsd.k12.ny.us>
2018-05-21 10:58:38 -0400

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,7 +1,7 @@
 // Add active class to the current button
 var navBar = document.getElementById("menu");
 var btns = navBar.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
