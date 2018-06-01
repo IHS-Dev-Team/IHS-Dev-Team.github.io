7399ab19e989ed35ad6492999e70515ee44c78e6
Vivek Myers <vivek.myers@gmail.com>
2018-05-31 21:16:59 -0400

    Margins

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
     if (prevScrollpos < currentScrollPos || currentScrollPos <= 0) {
-        document.getElementById("navbar").style.top = "0px";
+        document.getElementById("navbar").style.top = "5px";
     }
     else {
         document.getElementById("navbar").style.top = "-75px";
