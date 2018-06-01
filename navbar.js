71d1d92752531aab232faf570f5c644a542e6a55
Vivek Myers <vivek.myers@gmail.com>
2018-05-31 21:22:14 -0400

    Fix

--- navbar.js
+++ navbar.js
@@ -1,7 +1,7 @@
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos < currentScrollPos || currentScrollPos <= 0) {
+    if (prevScrollpos >= currentScrollPos || currentScrollPos <= 0) {
         document.getElementById("navbar").style.top = "5px";
     }
     else {
